10846589b9c67348c6e59e64035a2dd26f65faad
vijayhandal <handalvijay9588@gmail.com>
2022-07-28 12:15:29 +0530

    first commit

---
+++ Assignment 1/Details.java
@@ -0,0 +1,21 @@
+class Details{
+
+public static void main(String[] args){
+
+String name = "Vijay";
+String Father_Name = "Santosh";
+String Mother_Name = "Tulsa";
+int age = 23;
+String gender = "Male";
+String add = "Pune";
+String mobile = "9657625865";
+
+System.out.println("Name : "+name);
+System.out.println("Father’s Name : "+Father_Name);
+System.out.println("Mother‘s Name : "+Mother_Name);
+System.out.println("Age :"+age);
+System.out.println("Gender : "+gender);
+System.out.println("Address : "+add);
+System.out.println("Mobile No. : "+mobile);
+}
+}
